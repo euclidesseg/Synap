bb93fc785745c6724ce877bca7874c81ec2351e0
euclidesseg <euclides2696@gmail.com>
2026-01-25 22:15:14 -0500

    start construction editor

--- src/app/synap/pages/edit-page/edit-page.component.ts
+++ src/app/synap/pages/edit-page/edit-page.component.ts
@@ -1,42 +1,14 @@
 import { Component, AfterViewInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import Quill from 'quill';
+
 
 
 
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
-  providers: [
-    FormsModule
-  ]  
 })
-export default class EditPageComponent implements AfterViewInit {
+export default class EditPageComponent {
 
-  private quill!: Quill; // 🔹 Declaramos la variable
-
-  ngAfterViewInit(): void {
-    this.quill = new Quill('#editor', {
-      theme: 'snow',
-      modules: {
-        toolbar: [
-          [{ font: [] }, { size: [] }],                // Fuente y tamaño
-          [{ header: [1, 2, 3, false] }],              // H1 H2 H3
-          ['bold', 'italic', 'underline', 'strike'],   // Formato básico
-          [{ color: [] }, { background: [] }],         // Colores
-          [{ script: 'sub' }, { script: 'super' }],    // x²
-          [{ list: 'ordered' }, { list: 'bullet' }],   // Listas
-          [{ indent: '-1' }, { indent: '+1' }],        // Sangría
-          [{ align: [] }],                             // Alineación
-          ['blockquote', 'code-block'],                // Citas / código
-          ['link', 'image', 'video'],                  // Multimedia
-          ['clean'],                                    // Quitar formato
-          ['table'],
-        ],
-        table: true,
-        tableUI: true
-      },
-    });
-  }
+  
 
 }
