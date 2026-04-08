81d6d4e00632be1b9154cd190880d0c1e6dda362
euclidesseg <euclides2696@gmail.com>
2026-04-07 21:55:58 -0500

    add component editor

---
+++ src/app/synap/components/editor-component/editor-component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'editor-component',
+  imports: [],
+  templateUrl: './editor-component.html',
+})
+export class EditorComponent { }
