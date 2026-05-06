f2f3feda3bc838bd98aa9fe9ce308fd881e47169
euclidesseg <euclides2696@gmail.com>
2026-05-05 22:27:58 -0500

    Form design for publication editing begins

--- src/app/synap/pages/edit-page/edit-page.component.ts
+++ src/app/synap/pages/edit-page/edit-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, signal, inject, Renderer2, ElementRef, OnDestroy } from '@angular/core';
 import { EditorComponent } from "../../components/editor-component/editor-component";
 
 
@@ -8,9 +8,43 @@ import { EditorComponent } from "../../components/editor-component/editor-compon
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
   imports: [EditorComponent],
-})
-export default class EditPageComponent {
-
+  styles: `
   
+  `
+})
+export default class EditPageComponent implements OnDestroy {
 
+  private renderer = inject(Renderer2);
+  private elementRef = inject(ElementRef);
+  private removeListener!: () => void;
+
+  category = signal('Selecciona una opcion');
+  menuOpen = signal(false);
+
+  constructor() {
+    this.initClickListener();
+  }
+
+  showMenuOptions() {
+    this.menuOpen.update(value => !value);
+  }
+  selectOption(option: 'Ciencia' | 'Matematicas' | 'Ingenieria' | 'Computación' | 'Cine') {
+    this.category.set(option);
+    this.showMenuOptions();
+  }
+
+  private initClickListener() {
+    this.removeListener = this.renderer.listen('document', 'click',(event:MouseEvent) =>{
+      const clickedInside = this.elementRef.nativeElement.contains(event.target);
+
+      if(!clickedInside && this.menuOpen()){
+        this.menuOpen.set(false);
+      }
+    })
+  }
+  ngOnDestroy(): void {
+    if(this.removeListener){
+      this.removeListener();
+    }
+  }
 }
