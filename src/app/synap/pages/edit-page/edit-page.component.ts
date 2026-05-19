08589d6e8f9fc45d357d93b6659dcf063f615feb
euclidesseg <euclides2696@gmail.com>
2026-05-19 09:44:38 -0500

    init design of article form data

--- src/app/synap/pages/edit-page/edit-page.component.ts
+++ src/app/synap/pages/edit-page/edit-page.component.ts
@@ -1,13 +1,14 @@
 import { Component, AfterViewInit, signal, inject, Renderer2, ElementRef, OnDestroy } from '@angular/core';
 import { EditorComponent } from "../../components/editor-component/editor-component";
+import { FormArray, FormBuilder, FormControl, ReactiveFormsModule, Validators } from "@angular/forms";
 
 
-
+type Category = 'Ciencia' | 'Matematicas' | 'Ingenieria' | 'Computación' | 'Cine';
 
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
-  imports: [EditorComponent],
+  imports: [EditorComponent, ReactiveFormsModule],
   styles: `
   
   `
@@ -18,8 +19,37 @@ export default class EditPageComponent implements OnDestroy {
   private elementRef = inject(ElementRef);
   private removeListener!: () => void;
 
-  category = signal('Selecciona una opcion');
+  fBuilder:FormBuilder = inject(FormBuilder);
+
+  categoryOptions = signal<Category[]>(['Ciencia', 'Matematicas', 'Ingenieria', 'Computación', 'Cine']);
+
+  newLabel:FormControl = new FormControl('', [Validators.minLength(2)]);
+
+  articleForm = this.fBuilder.group({
+    title:['', [Validators.required]],
+    category:['', [Validators.required]],
+    labels:this.fBuilder.array(
+      [
+
+      ],
+      Validators.minLength(2)
+    )
+  });
+
+  get labels(){
+    return this.articleForm.get('labels') as FormArray;
+  }
+  onAddLabel(){
+    if(this.newLabel.invalid)return;
+    const newLabel:any = this.newLabel.value;
+    this
+    .labels.push(this.fBuilder.control(newLabel)
+    )
+
+  }
+  category = signal('');
   menuOpen = signal(false);
+  status = signal('');
 
   constructor() {
     this.initClickListener();
@@ -28,7 +58,7 @@ export default class EditPageComponent implements OnDestroy {
   showMenuOptions() {
     this.menuOpen.update(value => !value);
   }
-  selectOption(option: 'Ciencia' | 'Matematicas' | 'Ingenieria' | 'Computación' | 'Cine') {
+  selectOption(option: Category) {
     this.category.set(option);
     this.showMenuOptions();
   }
@@ -46,5 +76,9 @@ export default class EditPageComponent implements OnDestroy {
     if(this.removeListener){
       this.removeListener();
     }
+  }
+
+  onDeleteLabel(index:number){
+   this.labels.removeAt(index);
   }
 }
